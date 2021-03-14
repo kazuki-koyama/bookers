b087863ebbc597f3bd72a1071fec944d0605f890
kazuki-koyama <kazuk.koyama92@gmail.com>
2021-03-14 05:22:08 +0000

    [Add] editページ+update機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[])
+    @book = Book.find(params[:id])
   end
 
   def create
@@ -15,6 +15,12 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   private
