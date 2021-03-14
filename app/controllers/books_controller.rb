82cf4ffa8bf3667682a9a47cbc6150a90a3cde42
kazuki-koyama <kazuk.koyama92@gmail.com>
2021-03-14 08:05:07 +0000

    [Add] フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,12 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      redirect_to book_path(book.id), notice: "Book was successfully created."
+    else
+      flash.now[:danger] = "登録に失敗しました"
+      render action: :index
+    end
   end
 
   def edit
@@ -19,10 +23,14 @@ class BooksController < ApplicationController
   end
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      redirect_to book_path(book.id), notice: "Book was successfully updated."
+    else
+      flash.now[:danger] = "更新に失敗しました"
+      render action: :edit
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
